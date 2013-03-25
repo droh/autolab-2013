01766545459249843491ce6be028bd4ca664e9e9
Xuetao Guan <xuetao.guan@qq.com>
2013-03-25 19:32:33 -0400

    Add DhcpDns handling for tashi on single machine
    Signed-off-by: Xuetao Guan <xuetao.guan@qq.com>

--- tashi.sh
+++ tashi.sh
@@ -110,6 +110,23 @@ tashi_init()
 	# 2.3: Use $AUTOLAB/etc instead of /etc
 	sed -i "/nicString =/ s:/etc/qemu-ifup:$AUTOLAB&:"			$PYQEMU
 
+	# 3: Enable DhcpDns
+	# 3.1: Update secret key
+	sed -i "/^dnsKeyName =/ s:name_of_dns_key_hostname:reefshark:"		$CFG_CM
+	sed -i "/^dhcpKeyName =/ s:OMAPI:reefshark:"				$CFG_CM
+	sed -i "/^dnsSecretKey =/ s:ABcdEf12GhIJKLmnOpQrsT==:IiMIY9C0IawK8TZAca7bCQ==:" $CFG_CM
+	sed -i "/^dhcpSecretKey =/ s:ABcdEf12GhIJKLmnOpQrsT==:IiMIY9C0IawK8TZAca7bCQ==:" $CFG_CM
+	# 3.2: Server and Domain configs
+	sed -i "/^dnsServer =/ s:1.2.3.4 53:192.168.2.1 53:"			$CFG_CM
+	sed -i "/^dnsDomain =/ s:tashi.example.com:vmNet2013:"			$CFG_CM
+	sed -i "/^dhcpServer =/ s:1.2.3.4:192.168.2.1:"				$CFG_CM
+	# 3.3: Change ipRange
+	sed -i "/^ipRange1 =/ s:1 = 172.16.128.2-172.16.255.254:0 = 192.168.2.128-192.168.2.254:" $CFG_CM
+	# 3.4: Disable reverseDns
+	sed -i "/^reverseDns =/ s:True:False:"					$CFG_CM
+	# 3.5: Enable hook on DhcpDns
+	sed -i "/^#hook1 =/ s:#hook1:hook1:"					$CFG_CM
+
 	git add .; cd -
 }
 
