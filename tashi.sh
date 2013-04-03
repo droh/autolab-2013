f83ded19df4bd2967600ebe83ad66c427eba956d
Xuetao Guan <xuetao.guan@qq.com>
2013-04-03 14:10:41 -0400

    tashi.sh: Add /usr/share/tashi handler
    Signed-off-by: Xuetao Guan <xuetao.guan@qq.com>

--- tashi.sh
+++ tashi.sh
@@ -112,3 +112,7 @@ export AUTOLAB=/opt/autolab-2013
 
 # Add tashi/src to PYTHONPATH
 export PYTHONPATH=$AUTOLAB/tashi/src
+if [ -d /usr/share/tashi ]; then
+	echo 'The /usr/share/tashi must be renamed or removed, to avoid conflict configuration.'
+	mv /usr/share/tashi /usr/share/tashi.backup
+fi
