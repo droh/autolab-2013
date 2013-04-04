5cdcd44aecb97a00df2a62cabba3bff9e1986587
Xuetao Guan <xuetao.guan@qq.com>
2013-04-04 11:05:40 -0400

    Bugfix: wrong tashi src dir
    Signed-off-by: Xuetao Guan <xuetao.guan@qq.com>

--- autolab.sh
+++ autolab.sh
@@ -3,7 +3,7 @@
 # Add autolab-2013 alias
 export AUTOLAB=/opt/autolab-2013
 # Add tashi/src to PYTHONPATH
-export PYTHONPATH=$AUTOLAB/tashi/src
+export PYTHONPATH=$AUTOLAB/build/tashi/src
 if [ -d /usr/share/tashi ]; then
 	echo 'The /usr/share/tashi must be renamed or removed, to avoid conflict configuration.'
 	mv /usr/share/tashi /usr/share/tashi.backup
