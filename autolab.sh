79402da5bbcb1f4a3d3cf926348352399d05ff2e
Xuetao Guan <xuetao.guan@qq.com>
2013-04-07 17:10:40 -0400

    FIXME: use python 2.7.4
    Signed-off-by: Xuetao Guan <xuetao.guan@qq.com>

--- autolab.sh
+++ autolab.sh
@@ -8,3 +8,4 @@ if [ -d /usr/share/tashi ]; then
 	echo 'The /usr/share/tashi must be renamed or removed, to avoid conflict configuration.'
 	mv /usr/share/tashi /usr/share/tashi.backup
 fi
+ln -sf /usr/local/bin/python /usr/bin/python
